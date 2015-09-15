106fb27255345fa7a0a260a89eb917f423d82122
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-09-15 16:34:08 +0200

    Null default options

--- src/opbeat.js
+++ src/opbeat.js
@@ -8,9 +8,9 @@ function Opbeat () {
   this.isInstalled = false
 
   this.options = {
-    orgId: 'b3eba3d11f6e4c3a9db52f477caa4fa2',
-    appId: 'e9797db8c7',
-    token: '6451721d51b6d95cf6c6b09498feafd865f1f976',
+    orgId: null,
+    appId: null,
+    token: null,
     VERSION: this.VERSION
   }
 
