1401e8636a5b794a12636627771ab925d1a4515c
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-09-15 15:10:19 +0200

    Format utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,17 +1,14 @@
-
 module.exports = {
-
   getViewPortInfo: function getViewPort () {
-    var e = document.documentElement;
-    var g = document.getElementsByTagName('body')[0],
-    var x = window.innerWidth || e.clientWidth || g.clientWidth,
+    var e = document.documentElement
+    var g = document.getElementsByTagName('body')[0]
+    var x = window.innerWidth || e.clientWidth || g.clientWidth
     var y = window.innerHeight || e.clientHeight || g.clientHeight
 
     return {
       width: x,
-    	height: y
+      height: y
     }
   }
-
 
 }
