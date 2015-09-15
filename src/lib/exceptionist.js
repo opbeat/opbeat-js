cf3374a861a200ba95fff0d5685bb2abba308356
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-09-15 16:54:37 +0200

    Tidy up of exception cleaning logic.

--- src/lib/exceptionist.js
+++ src/lib/exceptionist.js
@@ -42,38 +42,35 @@ module.exports = {
     var stacktrace, label, i
 
     var type = exception.type
-    var message = exception.message
+    var message = String(exception.message) | 'Script error'
     var fileurl = exception.fileurl
     var lineno = exception.lineno
     var frames = exception.frames
 
-    // In some instances message is not actually a string, no idea why,
-    // so we want to always coerce it to one.
-    message += ''
-
-    // Sometimes an exception is getting logged in Opbeat.com as
-    // <no message value>
-    // This can only mean that the message was falsey since this value
-    // is hardcoded into Opbeat.com itself.
-    // At this point, if the message is falsey, we bail since it's useless
-    if (type === 'Error' && !message) return
-
     if (frames && frames.length) {
-      fileurl = frames[0].filename || fileurl
       // Opbeat.com expects frames oldest to newest
       // and JS sends them as newest to oldest
       frames.reverse()
-      stacktrace = {frames: frames}
+
+      stacktrace = {
+        frames: frames
+      }
+
     } else if (fileurl) {
       stacktrace = {
         frames: [{
           filename: fileurl,
-          lineno: lineno,
-          in_app: true
+          lineno: lineno
         }]
       }
     }
 
+    // Overrride fileurl from first frame
+    if (frames && frames.length && frames[0].filename) {
+      fileurl = frames[0].filename
+    }
+
+    // Human readable label
     label = lineno ? message + ' at ' + lineno : message
 
     var data = {
