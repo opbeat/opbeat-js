4668b058326bd240763cc475eef8560ae51a1ceb
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-09-15 16:34:30 +0200

    Update example with working Opbeat config

--- example/example.js
+++ example/example.js
@@ -1,26 +1,27 @@
 // Configure client
 Opbeat.config({
-    orgId: '21e3157338024d01bcd23d554436497c',
-    appId: 's2c204094d'
-}).install();
+  orgId: 'b3eba3d11f6e4c3a9db52f477caa4fa2',
+  appId: 'e9797db8c7',
+  token: '6451721d51b6d95cf6c6b09498feafd865f1f976'
+}).install()
 
 // Set optional user data
 Opbeat.setUserContext({
-    email: 'vanja@opbeat.com',
-    id: 1
+  email: 'vanja@opbeat.com',
+  id: 1
 })
 
 // Test functions
-function multiply(a, b) {
-    "use strict";
-    return a * b;
+function multiply (a, b) {
+  'use strict'
+  return a * b
 }
 
-function divide(a, b) {
-    "use strict";
-    try {
-        return multiply(add(a, b), a, b) / c;
-    } catch (e) {
-        Opbeat.captureException(e);
-    }
+function divide (a, b) {
+  'use strict'
+  try {
+    return multiply(add(a, b), a, b) / c
+  } catch (e) {
+    Opbeat.captureException(e)
+  }
 }
