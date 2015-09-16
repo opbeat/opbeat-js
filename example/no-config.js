81a08071178ee15c1b5d84df3af757763222cca0
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-09-16 15:11:25 +0200

    Add example of how the no config scenario works

---
+++ example/no-config.js
@@ -0,0 +1,18 @@
+// Test functions
+function multiply (a, b) {
+  'use strict'
+  return a * b
+}
+
+function divide (a, b) {
+  'use strict'
+  try {
+    return multiply(add(a, b), a, b) / c
+  } catch (e) {
+    Opbeat.captureException(e)
+  }
+}
+
+document.querySelector('.btn-test4').addEventListener('click', function () {
+  divide(123 / 2)
+}, false)
