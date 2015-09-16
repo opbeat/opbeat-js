0a29bd22fbe02d74210291f8bf4ed9039f97af47
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-09-16 15:11:11 +0200

    Add logic to extract config from <script data-*> attributes upon initialization

--- src/lib/config.js
+++ src/lib/config.js
@@ -14,7 +14,17 @@ module.exports = {
     }
 
     var config = defaults
+    var data = this.getConfigFromScript()
+    if (data) {
+      config = utils.mergeObject(defaults, data)
+    }
 
+    return config
+  },
+
+  getConfigFromScript: function () {
+    var script = utils.getCurrentScript()
+    var config = this.getDataAttributesFromNode(script)
     return config
   },
 
@@ -26,5 +36,29 @@ module.exports = {
 
     return values.indexOf(true) === -1
   },
+
+  getDataAttributesFromNode: function (node) {
+    var dataAttrs = {}
+    var dataRegex = /^data\-([\w\-]+)$/
+
+    if (node) {
+      var attrs = node.attributes
+      for (var i = 0; i < attrs.length; i++) {
+        var attr = attrs[i]
+        if (dataRegex.test(attr.nodeName)) {
+          var key = attr.nodeName.match(dataRegex)[1]
+
+          // Camel case key
+          key = key.split('-').map(function (group, index) {
+            return index > 0 ? group.charAt(0).toUpperCase() + group.substring(1) : group
+          }).join('')
+
+          dataAttrs[key] = attr.value || attr.nodeValue
+        }
+      }
+    }
+
+    return dataAttrs
+  }
 
 }
