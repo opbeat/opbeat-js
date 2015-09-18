14259aa04a6863c62592ddc618b9623be2663194
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-09-18 13:09:12 +0200

    Update web pack example to also minify via UgilifyJS

--- example/sourcemaps-example/webpack.config.js
+++ example/sourcemaps-example/webpack.config.js
@@ -1,13 +1,27 @@
 var path = require('path')
+var webpack = require('webpack')
 
 var BUILD_DIR = path.join(__dirname, 'build')
 
-var config =
-{
-  context: path.join(__dirname, 'src'),     devServer: { contentBase: BUILD_DIR },      devtool: '#inline-source-map',     entry: './scripts/index.js',     module: { loaders: [ { test: /index\.html$/,               loader: 'file-loader?name=[path][name].[ext]&context=./src'
-    }
-    ]
-  },     output: { path: BUILD_DIR,         filename: 'bundle.js'
+var config = {
+  context: path.join(__dirname, 'src'),
+  devServer: {
+  	contentBase: BUILD_DIR
+  },
+	devtool: '#source-map',
+	entry: './scripts/index.js',
+	module: {
+		loaders: [{
+			test: /index\.html$/,
+			loader: 'file-loader?name=[path][name].[ext]&context=./src'
+		}]
+  },
+  plugins: [
+      new webpack.optimize.UglifyJsPlugin()
+  ],
+  output: {
+  	path: BUILD_DIR,
+  	filename: 'bundle.js'
   }
 }
 
