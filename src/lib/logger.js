ddcc9e18d09c3ec882938079130c02a05863d3b7
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-10-01 11:03:24 +0200

    Add internal logstack in logger

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,10 +1,16 @@
 var config = require('./config')
 
+var logStack = []
+
 module.exports = {
   log: function (message, data) {
     var isDebugMode = config.get('debug') == 'true'
     var hasConsole = window.console
 
+    logStack.push({
+      msg: message,
+      data: data
+    })
 
     if (isDebugMode && hasConsole) {
       window.console.log(message, data)
