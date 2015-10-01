2e75cde0ee4de6b7426e09e9ffa1f834744cd6c9
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-10-01 11:03:32 +0200

    Add method to get logStack from logger

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -3,6 +3,10 @@ var config = require('./config')
 var logStack = []
 
 module.exports = {
+  getLogStack: function () {
+    return logStack
+  },
+
   log: function (message, data) {
     var isDebugMode = config.get('debug') == 'true'
     var hasConsole = window.console
