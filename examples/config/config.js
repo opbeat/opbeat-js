5ef24b8141008069657ac7b160f7e62517fb774d
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-10-05 10:15:46 +0200

    Revamped examples.

--- examples/config/config.js
+++ examples/config/config.js
@@ -11,16 +11,3 @@ Opbeat.setUserContext({
   email: 'vanja@opbeat.com',
   id: 1
 })
-
-// Test functions
-function multiply (a, b) {
-  return a * b
-}
-
-function divide (a, b) {
-  return multiply(add(a, b), a, b) / c
-}
-
-document.querySelector('.btn-test4').addEventListener('click', function () {
-  divide(123 / 2)
-}, false)
