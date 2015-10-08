3c3f5fbf50cba2719d48a2ba5f06f9b6fe149ffe
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-10-08 11:50:12 +0200

    Update app id for Angular example

--- examples/angular/js/app.js
+++ examples/angular/js/app.js
@@ -33,7 +33,7 @@ angular.module('todomvc', ['ngRoute', 'ngOpbeat'])
     $opbeatProvider.config({
       debug: true,
       orgId: 'b3eba3d11f6e4c3a9db52f477caa4fa2',
-      appId: 'e9797db8c7',
+      appId: 'a7971dbd71'
     })
 
     $opbeatProvider.install()
