4d124306aca9cec643e958b81b3432c357f10b66
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-10-22 15:20:18 +0200

    Instrument $httpBackend

--- src/angular-opbeat.js
+++ src/angular-opbeat.js
@@ -211,6 +211,29 @@ function $opbeatInstrumentationProvider ($provide) {
     };
   })
 
+  // $httpBackend instrumentation
+  $provide.decorator('$httpBackend', function ($delegate, $injector) {
+
+    var $rootScope = $injector.get('$rootScope')
+    var $location = $injector.get('$location')
+
+    return function () {
+      var args = Array.prototype.slice.call(arguments)
+      var transaction = $rootScope._opbeatTransactions && $rootScope._opbeatTransactions[$location.absUrl()]
+
+      var result = utils.instrumentMethodWithCallback($delegate, '$httpBackend', transaction, 'app.httpBackend', {
+        prefix: '$httpBackend',
+        callbackIndex: 3,
+        signatureFormatter: function(key, args) {
+          var text = ['$httpBackend', args[0].toUpperCase(), args[1]]
+          return text.join(' ')
+        }
+      }).apply(this, args)
+
+      return result
+    };
+  })
+
   // $cacheFactory instrumentation
   $provide.decorator('$cacheFactory', function ($delegate, $injector) {
     return function () {
