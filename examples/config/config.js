a12c930de0c718a27a3cf0cb1e89cce803cd3cef
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-10-23 15:57:04 +0200

    New async API

--- examples/config/config.js
+++ examples/config/config.js
@@ -1,12 +1,15 @@
-// Configure client
-Opbeat.config({
+
+_opbeat = window._opbeat || function() {
+  (window._opbeat.q = window._opbeat.q || []).push(arguments)
+};
+
+_opbeat('config', {
   debug: true,
   orgId: 'b3eba3d11f6e4c3a9db52f477caa4fa2',
   appId: 'e9797db8c7'
-}).install()
+});
 
-// Set optional user data
-Opbeat.setUserContext({
+_opbeat('setUserContext', {
   email: 'vanja@opbeat.com',
   id: 1
-})
+});
