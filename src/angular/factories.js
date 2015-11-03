80a378acded171cdb6ea6e6e801c84838ac24e91
kenneth <kenneth@Kirstens-MacBook-Air.local>
2015-11-03 12:24:11 +0100

    Add missing factories.js

---
+++ src/angular/factories.js
@@ -0,0 +1,17 @@
+var utils = require('../instrumentation/utils')
+
+module.exports = function($provide, traceBuffer) {
+  return {
+    instrumentationAll: function(modules) {
+      modules.forEach(function (name) {
+        $provide.decorator(name, function ($delegate, $injector) {
+          utils.instrumentObject($delegate, $injector, {
+            type: 'angular.factory',
+            prefix: name
+          })
+          return $delegate
+        })
+      })
+    }    
+  }
+}
