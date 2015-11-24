96e769b6dd8817d25984ba500925b3dd42f665d5
auchenberg <kenneth@auchenberg.dk>
2015-11-24 14:32:21 +0100

    Make instrumentation of templateRequest optional to gain Angular 1.2 support

--- src/instrumentation/angular/templateRequest.js
+++ src/instrumentation/angular/templateRequest.js
@@ -1,15 +1,18 @@
 var utils = require('../utils')
 
 module.exports = function ($provide, traceBuffer) {
-  // Template Request Instrumentation
-  $provide.decorator('$templateRequest', ['$delegate', '$injector', function ($delegate, $injector) {
-    return utils.instrumentModule($delegate, $injector, {
-      type: 'template.angular.request',
-      prefix: '$templateRequest',
-      signatureFormatter: function (key, args) {
-        var text = ['$templateRequest', args[0]]
-        return text.join(' ')
-      }
-    })
-  }])
+  try { 
+    // Template Request Instrumentation
+    $provide.decorator('$templateRequest', ['$delegate', '$injector', function ($delegate, $injector) {
+      return utils.instrumentModule($delegate, $injector, {
+        type: 'template.angular.request',
+        prefix: '$templateRequest',
+        signatureFormatter: function (key, args) {
+          var text = ['$templateRequest', args[0]]
+          return text.join(' ')
+        }
+      })
+    }])
+  } catch (e) {
+  }
 }
