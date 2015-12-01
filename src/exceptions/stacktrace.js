74608ebfee7b12ca83ed39b7e01ed3babf19afdc
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-12-01 14:16:33 +0100

    Filter out “opbeatFunctionWrapper” when generating stacktraces

--- src/exceptions/stacktrace.js
+++ src/exceptions/stacktrace.js
@@ -10,6 +10,7 @@ var defaultOptions = {
     return (stackframe.functionName || '').indexOf('StackTrace$$') === -1 &&
       (stackframe.functionName || '').indexOf('ErrorStackParser$$') === -1 &&
       (stackframe.functionName || '').indexOf('StackGenerator$$') === -1 &&
+      (stackframe.functionName || '').indexOf('opbeatFunctionWrapper') === -1 &&
       (stackframe.fileName || '').indexOf('angular-opbeat.js') === -1 &&
       (stackframe.fileName || '').indexOf('angular-opbeat.min.js') === -1 &&
       (stackframe.fileName || '').indexOf('opbeat.js') === -1 &&
