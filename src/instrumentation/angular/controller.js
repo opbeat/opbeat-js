64f6faadc8ea52658ef1d2c44a1ab3865d5af2f6
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-12-04 15:51:29 -0500

    Instrument all controllers

--- src/instrumentation/angular/controller.js
+++ src/instrumentation/angular/controller.js
@@ -7,16 +7,11 @@ module.exports = function ($provide, traceBuffer) {
     return function () {
       var args = Array.prototype.slice.call(arguments)
       var controllerInfo = utils.getControllerInfoFromArgs(args)
-      var transaction = transactionStore.getRecentByUrl($injector.get('$location').absUrl())
 
-      if (controllerInfo.name) {
-        if (transaction && transaction.metadata.controllerName !== controllerInfo.name) {
-          return utils.instrumentModule($delegate, $injector, {
-            type: 'app.$controller',
-            prefix: '$controller.' + controllerInfo.name
-          }).apply(this, arguments)
-        }
-      }
+      return utils.instrumentModule($delegate, $injector, {
+        type: 'app.$controller',
+        prefix: '$controller.' + controllerInfo.name
+      }).apply(this, arguments)
 
       return $delegate.apply(this, args)
     }
