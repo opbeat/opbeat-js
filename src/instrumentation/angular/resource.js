990b4bb968778e1d6c0193d0167b04b5e9a4e565
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-12-14 14:07:32 +0100

    Use instrumentStaticModule for $resource instrumentation

--- src/instrumentation/angular/resource.js
+++ src/instrumentation/angular/resource.js
@@ -4,21 +4,14 @@ module.exports = function ($provide, traceBuffer) {
   try {
     // ngResource instrumentation
     $provide.decorator('$resource', ['$delegate', '$injector', function ($delegate, $injector) {
-      return function () {
-        var args = Array.prototype.slice.call(arguments)
-        var result = $delegate.apply(this, args)
-        utils.instrumentObject(result, $injector, {
-          type: 'ext.$resource',
-          prefix: '$resource',
-          traceBuffer: traceBuffer,
-          signatureFormatter: function (key, args) {
-            var text = ['$resource', key.toUpperCase(), args[0]]
-            return text.join(' ')
-          }
-        })
-        return result
-      }
+      return utils.instrumentStaticModule($delegate, $injector, {
+        traceBuffer: traceBuffer,
+        prefix: '$resource',
+        type: 'ext.$resource',
+        signatureFormatter: function (key, args) {
+          return ['$resource', key.toUpperCase(), args[0]].join(' ')
+        }
+      })
     }])
-  } catch (e) {
-  }
+  } catch (e) {}
 }
