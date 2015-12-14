f2199a0ba60bfc083fa0f71688b263c6f16411bc
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-12-14 14:06:54 +0100

    Use instrumentStaticModule for $cacheFactory instrumentation

--- src/instrumentation/angular/cacheFactory.js
+++ src/instrumentation/angular/cacheFactory.js
@@ -3,20 +3,19 @@ var utils = require('../utils')
 module.exports = function ($provide, traceBuffer) {
   // $cacheFactory instrumentation (this happens before routeChange -> using traceBuffer)
   $provide.decorator('$cacheFactory', ['$delegate', '$injector', function ($delegate, $injector) {
-    return function () {
-      var args = Array.prototype.slice.call(arguments)
-      var cacheName = args[0] + 'Cache'
-      var result = $delegate.apply(this, args)
-      utils.instrumentObject(result, $injector, {
-        type: 'cache.' + cacheName,
-        prefix: cacheName,
-        traceBuffer: traceBuffer,
-        signatureFormatter: function (key, args) {
-          var text = ['$cacheFactory', key.toUpperCase(), args[0]]
-          return text.join(' ')
-        }
-      })
-      return result
-    }
+
+    return utils.instrumentStaticModule($delegate, $injector, {
+      traceBuffer: traceBuffer,
+      prefix: function(args) {
+        return args[0] + 'Cache'
+      },
+      type: function() {
+        return 'cache.' + this.prefix
+      },
+      signatureFormatter: function (key, args, options) {
+        return ['$' + options.prefix, key.toUpperCase(), args[0]].join(' ')
+      }
+    })
+
   }])
 }
