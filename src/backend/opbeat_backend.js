5f262897a11d711b6890334324719df4e09fd52f
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2016-03-03 10:57:13 +0100

    add backend/opbeat_backend

---
+++ src/backend/opbeat_backend.js
@@ -0,0 +1,161 @@
+module.exports = OpbeatBackend
+function OpbeatBackend (transport, logger) {
+  this._logger = logger
+  this._transport = transport
+}
+
+OpbeatBackend.prototype.sendTransactions = function (transactionList) {
+  var formatedTransactions = this._formatTransactions(transactionList)
+  return this._transport.sendTransaction(formatedTransactions)
+}
+OpbeatBackend.prototype._formatTransactions = function (transactionList) {
+  var transactions = this.groupTransactions(transactionList)
+
+  var traces = [].concat.apply([], transactionList.map(function (trans) {
+    return trans.traces
+  }))
+
+  var groupedTraces = groupTraces(traces)
+  var groupedTracesTimings = this.getRawGroupedTracesTimings(traces, groupedTraces)
+
+  return {
+    transactions: transactions,
+    traces: {
+      groups: groupedTraces,
+      raw: groupedTracesTimings
+    }
+  }
+}
+
+OpbeatBackend.prototype.groupTransactions = function groupTransactions (transactions) {
+  var groups = grouper(transactions, transactionGroupingKey)
+  return Object.keys(groups).map(function (key) {
+    var trans = groups[key][0]
+    var durations = groups[key].map(function (trans) {
+      return trans.duration()
+    })
+    return {
+      transaction: trans.name,
+      result: trans.result,
+      kind: trans.type,
+      timestamp: groupingTs(trans._startStamp).toISOString(),
+      durations: durations
+    }
+  })
+}
+
+OpbeatBackend.prototype.getRawGroupedTracesTimings = function getRawGroupedTracesTimings (traces, groupedTraces) {
+  var getTraceGroupIndex = function (col, item) {
+    var index = 0
+    var targetGroup = traceGroupingKey(item)
+
+    col.forEach(function (item, i) {
+      if (item._group === targetGroup) {
+        index = i
+      }
+    })
+
+    return index
+  }
+
+  var groupedByTransaction = grouper(traces, function (trace) {
+    return trace.transaction.name + '|' + trace.transaction._start
+  })
+
+  return Object.keys(groupedByTransaction).map(function (key) {
+    var traces = groupedByTransaction[key]
+    var transaction = traces[0].transaction
+
+    var data = [transaction.duration()]
+
+    traces.forEach(function (trace) {
+      var groupIndex = getTraceGroupIndex(groupedTraces, trace)
+      var relativeTraceStart = trace._start - transaction._start
+
+      if (relativeTraceStart > transaction.duration()) {
+        this._logger.debug('%c -- opbeat.instrumentation.getRawGroupedTracesTimings.error.relativeTraceStartLargerThanTransactionDuration', 'color: #ff0000', relativeTraceStart, transaction._start, transaction.duration(), { trace: trace, transaction: transaction })
+      } else if (relativeTraceStart < 0) {
+        this._logger.debug('%c -- opbeat.instrumentation.getRawGroupedTracesTimings.error.negativeRelativeTraceStart!', 'color: #ff0000', relativeTraceStart, trace._start, transaction._start, trace)
+      } else if (trace.duration() > transaction.duration()) {
+        this._logger.debug('%c -- opbeat.instrumentation.getRawGroupedTracesTimings.error.traceDurationLargerThanTranscationDuration', 'color: #ff0000', trace.duration(), transaction.duration(), { trace: trace, transaction: transaction })
+      } else {
+        data.push([groupIndex, relativeTraceStart, trace.duration()])
+      }
+    })
+
+    return data
+  })
+}
+
+function groupTraces (traces) {
+  var groupedByMinute = grouper(traces, traceGroupingKey)
+
+  return Object.keys(groupedByMinute).map(function (key) {
+    var trace = groupedByMinute[key][0]
+
+    var startTime = trace._start
+    if (trace.transaction) {
+      startTime = startTime - trace.transaction._start
+    } else {
+      startTime = 0
+    }
+
+    return {
+      transaction: trace.transaction.name,
+      signature: trace.signature,
+      kind: trace.type,
+      timestamp: trace._startStamp.toISOString(),
+      parents: trace.ancestors(),
+      extra: {
+        _frames: trace.frames
+      },
+      _group: key
+    }
+  }).sort(function (a, b) {
+    return a.start_time - b.start_time
+  })
+}
+
+function grouper (arr, func) {
+  var groups = {}
+
+  arr.forEach(function (obj) {
+    var key = func(obj)
+    if (key in groups) {
+      groups[key].push(obj)
+    } else {
+      groups[key] = [obj]
+    }
+
+    obj._traceGroup = key
+  })
+
+  return groups
+}
+
+function groupingTs (ts) {
+  return new Date(ts.getFullYear(), ts.getMonth(), ts.getDate(), ts.getHours(), ts.getMinutes())
+}
+
+function transactionGroupingKey (trans) {
+  return [
+    groupingTs(trans._startStamp).getTime(),
+    trans.name,
+    trans.result,
+    trans.type
+  ].join('-')
+}
+
+function traceGroupingKey (trace) {
+  var ancestors = trace.ancestors().map(function (trace) {
+    return trace.signature
+  }).join(',')
+
+  return [
+    groupingTs(trace._startStamp).getTime(),
+    trace.transaction.name,
+    ancestors,
+    trace.signature,
+    trace.type
+  ].join('-')
+}
