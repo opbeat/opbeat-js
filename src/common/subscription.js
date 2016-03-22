0a6fbe9abbfe7467bfdc6eddfe7bc14ba8634945
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2016-03-22 11:54:56 +0100

    add Common.Subscription

---
+++ src/common/subscription.js
@@ -0,0 +1,25 @@
+function Subscription () {
+  this.subscriptions = []
+}
+
+Subscription.prototype.subscribe = function (fn) {
+  var self = this
+  this.subscriptions.push(fn)
+
+  return function () {
+    var index = self.subscriptions.indexOf(fn)
+    if (index > -1) {
+      self.subscriptions.splice(index, 1)
+    }
+  }
+}
+
+Subscription.prototype.applyAll = function (applyTo, applyWith) {
+  this.subscriptions.forEach(function (fn) {
+    try {
+      fn.apply(applyTo, applyWith)
+    } catch (error) {}
+  }, this)
+}
+
+module.exports = Subscription
