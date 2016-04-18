88f15a3cc30f8253b68af19ca9c30fa5ec103fda
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2016-04-18 16:10:20 +0200

    temporarily get rid of zone.js deprecation warning

--- src/angular/serviceContainer.js
+++ src/angular/serviceContainer.js
@@ -3,20 +3,24 @@ var ngOpbeat = require('./ngOpbeat')
 var TransactionService = require('../transaction/transaction_service')
 
 function ServiceContainer (config) {
-  this.services = {logger: logger}
+  this.services = { logger: logger }
   logger.setLevel(config.logLevel, false)
   var transactionService = this.services.transactionService = new TransactionService(logger, {})
+  // todo: remove this when updating to new version of zone.js
+  function noop () { }
+  var _warn = console.warn
+  console.warn = noop
 
   if (typeof window.zone === 'undefined') {
     require('zone.js')
   }
 
-  function noop () {}
   var zonePrototype = ('getPrototypeOf' in Object)
     ? Object.getPrototypeOf(window.zone) : window.zone.__proto__ // eslint-disable-line 
 
   zonePrototype.enqueueTask = noop
   zonePrototype.dequeueTask = noop
+  console.warn = _warn
 
   var ZoneService = require('../transaction/zone_service')
   var zoneService = this.services.zoneService = new ZoneService(window.zone, transactionService, logger)
