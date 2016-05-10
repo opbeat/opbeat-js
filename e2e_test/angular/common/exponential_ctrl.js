3afed48d8347e7fba6cfee8d0e2722c502d3b63e
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2016-05-10 11:26:48 +0200

    e2e click interaction

--- e2e_test/angular/common/exponential_ctrl.js
+++ e2e_test/angular/common/exponential_ctrl.js
@@ -10,11 +10,18 @@ function exponentialCtrl ($scope, $http) {
   $scope.confirmation = function (conf) {
     $scope.confirmation = conf
   }
+  $scope.fetchExtraData = function () {
+    $http.get('common/confirmation.json').then(function (response) {
+      $scope.extraData = exponentialize(repeatArray, 3)
+    }, function () {
+      throw new Error('Confirmation failed.')
+    })
+  }
 
   var repeatArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
   $scope.repeatArray = repeatArray
-  $scope.justAnObject = {key: 'value'}
+  $scope.justAnObject = { key: 'value' }
   $scope.returnArrayFn = function () {
     return repeatArray
   }
