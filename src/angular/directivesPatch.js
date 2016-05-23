40a4a18798887968708bfd8c6bdb58e7ad2d304a
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2016-05-23 18:13:08 +0200

    clean up directivesPatch

--- src/angular/directivesPatch.js
+++ src/angular/directivesPatch.js
@@ -45,33 +45,6 @@ module.exports = function ($provide, transactionService) {
     }
     return $delegate
   }])
-
-  // function patchEventDirective (delegate, eventName) {
-  //   var nativeCompile = delegate.compile
-  //   delegate.compile = function () {
-  //     var nativeLink = nativeCompile.apply(this, arguments)
-  //     return function (scope, element, attributes) {
-  //       var directiveName = attributes.$normalize('ng-' + eventName)
-  //       var action = attributes[directiveName]
-  //       element.on(eventName, function (event) {
-  //         transactionService.startTransaction(directiveName + ': ' + action, 'transaction')
-  //       })
-  //       return nativeLink.apply(this, arguments)
-  //     }
-  //   }
-  // }
-
-  // $provide.decorator('ngSubmitDirective', ['$delegate', '$injector', function ($delegate, $injector) {
-  //   var directive = $delegate[0]
-  //   patchEventDirective(directive, 'submit')
-  //   return $delegate
-  // }])
-
-  // $provide.decorator('ngClickDirective', ['$delegate', '$injector', function ($delegate, $injector) {
-  //   var ngClick = $delegate[0]
-  //   patchEventDirective(ngClick, 'click')
-  //   return $delegate
-  // }])
 }
 
 function humanReadableWatchExpression (fn) {
