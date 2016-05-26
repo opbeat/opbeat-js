7455f9bc4b04abb945c5ca3a91ad029ba65c6255
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2016-05-26 15:26:32 +0200

    refactor fileFetcher, cache failed requests

--- src/lib/fileFetcher.js
+++ src/lib/fileFetcher.js
@@ -1,4 +1,3 @@
-var Promise = require('es6-promise').Promise
 var SimpleCache = require('simple-lru-cache')
 var transport = require('./transport')
 
@@ -6,56 +5,14 @@ var cache = new SimpleCache({
   'maxSize': 1000
 })
 
-var fetchQueue = {}
-
-var getFileFromNetwork = function (url) {
-  return new Promise(function (resolve, reject) {
-    if (fetchQueue[url]) {
-      fetchQueue[url].push(resolve)
-      return
-    }
-
-    fetchQueue[url] = []
-
-    transport.getFile(url).then(function (source) {
-      cache.set(url, source)
-      resolve(source)
-
-      if (fetchQueue[url]) {
-        fetchQueue[url].forEach(function (fn) {
-          fn()
-        })
-      }
-      fetchQueue[url] = null
-    }, function (err) {
-      fetchQueue[url] = null
-      reject(err)
-    })
-  })
-}
-
-var getFileFromCache = function (url, missCallback) {
-  return new Promise(function (resolve, reject) {
-    var cachedSource = cache.get(url)
-    if (cachedSource) {
-      resolve(cachedSource)
-    } else {
-      reject()
-    }
-  })
-}
-
 module.exports = {
-
   getFile: function (url) {
-    return new Promise(function (resolve, reject) {
-      getFileFromCache(url).then(function (fileSource) { // Try to get from cache
-        resolve(fileSource)
-      }, function () {
-        getFileFromNetwork(url).then(function (fileSource) { // then try network
-          resolve(fileSource)
-        }, reject) // give up
-      })
-    })
+    var cachedPromise = cache.get(url)
+    if (typeof cachedPromise !== 'undefined') {
+      return cachedPromise
+    }
+    var filePromise = transport.getFile(url)
+    cache.set(url, filePromise)
+    return filePromise
   }
 }
