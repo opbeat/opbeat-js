64d28d061c10207bf623c92946a1445cdfbd6e8e
Ron Cohen <cohen1@gmail.com>
2016-07-13 16:27:31 +0200

    end2end on travis (#65)
    * Making travisci run end2end tests
    
    `npm test` will now run the end to end tests on
    saucelabs in addition to the unit tests.
    
    * Remove Angular 1.2 as it's not supported yet.
    
    * Fix test on Angular 1.3
    
    * Attempt to retry the saucelab connection when it fails

--- gulpfile.js
+++ gulpfile.js
@@ -266,15 +266,24 @@ gulp.task('test:e2e:launchsauceconnect', function (done) {
     logger: console.log
   }
 
-  sauceConnectLauncher(config, function (err, sauceConnectProcess) {
-    if (err) {
-      console.error(err.message)
-      return process.exit(1)
-    }
+  var tryConnect = function(maxAttempts, currAttempts, done) {
+    sauceConnectLauncher(config, function (err, sauceConnectProcess) {
+      if (err) {
+        console.error(err.message)
+        if (currAttempts <= maxAttempts) {
+          console.log("Retrying... (attempt " + currAttempts + " of " + maxAttempts + ")")
+          tryConnect(maxAttempts, ++currAttempts, done)
+        }else{
+          return process.exit(1)
+        }
+      }else{
+        console.log('Sauce Connect ready')
+        done()
+      }
+    })
+  }
 
-    console.log('Sauce Connect ready')
-    done()
-  })
+  tryConnect(3, 1, done)
 })
 
 // Serve test application
@@ -314,8 +323,8 @@ gulp.task('test:e2e:start-sauce', function (done) {
   })
 })
 
-gulp.task('test:e2e:start-travis', function (done) {
-  runSequence('build', 'test:e2e:serve', 'test:e2e:sauceconnect', function () {
+gulp.task('test:e2e', function (done) {
+  runSequence('build', 'test:e2e:serve', 'test:e2e:launchsauceconnect', 'test:e2e:sauceconnect', function () {
     console.log('All tasks completed.')
     done()
     process.exit(0)
--- wdio.sauce.conf.js
+++ wdio.sauce.conf.js
@@ -4,7 +4,6 @@ config.capabilities = [
     maxInstances: 1,
     browserName: 'chrome',
     'version': '45',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     before: function () {
       browser.timeoutsAsyncScript(30000)
     }
